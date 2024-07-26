058f7425f38b0e5eeb65aa986d5f1d31ec0e28f8
hoipranav <hoipranavv@gmail.com>
2024-07-26 22:56:59 +0530

    Add comments scrape functionality

--- app/backend/data.py
+++ app/backend/data.py
@@ -4,23 +4,26 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from time import sleep
 
-def scrape_platform():
-    '''Scrape all the required data from the Paltform Post/Video'''
-    # display = Display(visible=0, size=(800, 600))
-    # display.start()
-    driver = webdriver.Chrome()
-    driver.maximize_window()
-
-    driver.get("https://www.youtube.com/watch?v=3OUdeW4HmgE&list=RD3OUdeW4HmgE&start_radio=1")
-    sleep(3)
-
-    driver.execute_script("window.scrollTo(0, 400);")
-
+def get_comments(url, driver):
+    '''Used to scrape comments from the web'''
+    driver.get(url)
+    sleep(1)
+    driver.execute_script("window.scrollTo(0, 7000);")
+    sleep(1)
     for i in range(5):
-        sleep(3)
-        driver.execute_script("window.scrollTo(0, 10000);")
-    sleep(3)
+        tags = []
+        driver.execute_script("window.scrollTo(0, 1000000);")
+        for i in driver.find_elements(By.XPATH, '//*[@id="content-text"]/span'):
+            tags.append(i.text)
+        sleep(0.1)
+    return tags
 
+def scrape_platform(url):
+    '''Set up Selenium & virtual display for automation'''
+    display = Display(visible=0, size=(800, 600))
+    display.start()
+    driver = webdriver.Chrome()
+    tags = get_comments("https://www.youtube.com/watch?v=3OUdeW4HmgE&list=RD3OUdeW4HmgE&start_radio=1", driver)
     driver.quit()
-
-scrape_platform()
+    display.stop()
+    return tags
