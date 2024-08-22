202f4fc2910002dd4ce4bc3722a3930e042a5e73
hoipranav <hoipranavv@gmail.com>
2024-08-23 00:00:09 +0530

    refactor: add some text files

--- analyzer/app/app.py
+++ analyzer/app/app.py
@@ -8,7 +8,7 @@ st.title("Welcome to Multiplatform-Post-Analyzer")
 url = st.text_input("Paste the link below.")
 
 dropdown = st.selectbox(
-    "Which paltform content would you like to analyze?",
+    "Which platform content would you like to analyze?",
     ("Youtube", "Reddit"),
     placeholder="Select the platform..."
 )
@@ -21,17 +21,19 @@ if st.button("Analyze"):
 
     if dropdown == "Youtube":
         response = requests.post('http://127.0.0.1:8000/scrape/youtube', json=elements).json()
-        with open('comments.txt', 'w') as comments_file:
+        with open('youtube_comments.txt', 'w') as comments_file:
             for i in response:
                 comments_file.write(f"{i}\n")
-        comments = clean_comments()
-        with open('clean_comments.txt', 'w') as file:
+        comments = clean_comments("youtube")
+        with open('clean_youtube_comments.txt', 'w') as file:
             for i in comments:
                 file.write(f"{i}\n")
     if dropdown == "Reddit":
         response = requests.post('http://127.0.0.1:8000/scrape/reddit', json=elements).json()
-        print(response)
-    # if dropdown == "LinkedIn":
-    #     response = requests.post('http://127.0.0.1:8000/scrape/linkedIn', json=elements).json()
-    #     for i in response:
-    #         print(i)
+        with open("reddit_comments.txt", 'w') as file:
+            for i in response:
+                file.write(f"{i}\n")
+        comments = clean_comments("reddit")
+        with open('clean_reddit_comments.txt', 'w') as file:
+            for i in comments:
+                file.write(f"{i}\n")
