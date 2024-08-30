a452f51d9dc64bdab1bbbb719007053fedfa211d
hoipranav <hoipranavv@gmail.com>
2024-08-30 16:24:29 +0530

    feat: add docker support

--- analyzer/app/app.py
+++ analyzer/app/app.py
@@ -1,6 +1,5 @@
 import streamlit as st
 import requests
-from analyzer.model.model_preprocessing import preprocessing
 
 
 st.title("Welcome to Platform-Analyzer")
@@ -16,14 +15,10 @@ dropdown = st.selectbox(
 
 def get_comments(platform:str):
     """Calls the post method according to the respective platform given as input"""
-    response = requests.post("http://127.0.0.1:8000/scrape/{0}".format(platform), json=elements).json()
+    response = requests.post("http://host.docker.internal:81/127.0.0.1:8000/scrape/{0}".format(platform), json=elements).json()
     with open(f"{platform}_comments.txt", 'w') as comments_file:
         for i in response:
             comments_file.write(f"{i}\n")
-    comments = preprocessing(platform)
-    with open(f"clean_{platform}_comments.txt", 'w') as file:
-        for i in comments:
-            file.write(f"{i}\n")
 
 
 if st.button("Analyze"):
