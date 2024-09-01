36b102abc5b71620123228605cb223860a5f6a75
tanmaygaikwad723 <gaikwadtanmay723@gmail.com>
2024-09-01 13:49:25 +0530

    Solve the 'failed to resolve host.docker.internal' error

--- analyzer/app/app.py
+++ analyzer/app/app.py
@@ -15,7 +15,8 @@ dropdown = st.selectbox(
 
 def get_comments(platform:str):
     """Calls the post method according to the respective platform given as input"""
-    response = requests.post("http://host.docker.internal:81/127.0.0.1:8000/scrape/{0}".format(platform), json=elements).json()
+    response = requests.post("http://0.0.0.0:8081/scrape/{0}".format(platform), json=elements).json()
+    # response = requests.post("{config.server.host}:81/127.0.0.1:8000/scrape/{0}".format(platform), json=elements).json()
     with open(f"{platform}_comments.txt", 'w') as comments_file:
         for i in response:
             comments_file.write(f"{i}\n")
